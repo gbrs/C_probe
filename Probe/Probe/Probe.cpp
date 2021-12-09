62974d43a25d1a407d8a89f5a442c2bebb5716df
gbrs <61385511+gbrs@users.noreply.github.com>
2021-12-09 11:32:08 +0300

    experimented with debugging

---
+++ Probe/Probe/Probe.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int Sum(int a, int b)
+{
+    int c = 2 * a + b;
+    return c;
+}
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int c, d;
+    c = a + b;
+    d = Sum(a, b);
+
+    cout << c << endl;
+}
+
