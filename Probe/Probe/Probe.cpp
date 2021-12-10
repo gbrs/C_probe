39d5fce710d623fceb1bafc9c19becc9343bb146
gbrs <61385511+gbrs@users.noreply.github.com>
2021-12-10 11:10:47 +0300

    experimented with debugging (arrays, functions, loop)

--- Probe/Probe/Probe.cpp
+++ Probe/Probe/Probe.cpp
@@ -14,7 +14,11 @@ int main()
     int c, d;
     c = a + b;
     d = Sum(a, b);
-
-    cout << c << endl;
+    char s[10] = "Hello";
+    int arr[5];
+    for (size_t i = 0; i < 5; i++)
+    {
+        arr[i] = i * i;
+    }
+    cout << s << endl;
 }
-
