b07a4b50e39cd3b0095c6d021c93ff415ced5090
gbrs <gbrs@mail.ru>
2022-02-12 10:13:45 +0300

    experimented with access to feld of structure in the field

--- Probe/Probe/Probe.cpp
+++ Probe/Probe/Probe.cpp
@@ -1,28 +1,31 @@
-﻿// дэбаггинг
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-int Sum(int u, int y)
+struct person
 {
-    int c = 2 * u + y;
-    return c;
+    int age;
+    char name[20];
+};
+
+int main(void)
+{
+    struct person people[] = { 23, "Tom", 32, "Bob", 26, "Alice", 41, "Sam" };
+
+    int n = sizeof(people) / sizeof(people[0]);
+
+    /*
+    for (struct person* p = people; p < people + n; p++)
+    {
+        cout << "name: " << p->name << " age: " << p->age << endl;
+    }
+    */
+    for (int i=0; i < n; i++)
+    {
+        cout << "name: " << people[i].name << " age: " << people[i].age << endl;
+    }
+    return 0;
 }
 
-int main()
-{
-    int a = 5;
-    int b = 6;
-    int c, d;
-    c = a + b;
-    d = Sum(a, b);
-    char s[10] = "Hello";
-    int arr[5];
-    for (size_t i = 0; i < 5; i++)
-    {
-        arr[i] = i * i;
-    }
-    cout << s << endl;
-}
 
 
 /*
